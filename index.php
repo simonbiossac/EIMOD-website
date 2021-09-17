cbe5f12f79617d31aca8a00fafdb11690ef39e29
simonbiossac <simonbiossac@gmail.com>
2021-09-17 02:05:47 +0200

    init

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" class="no-js">
+<head>
+	  <?php require './assets/inc/meta.inc';?>
+	  <link rel="stylesheet" href="./assets/css/master.min.css">
+	  <title>simonbiossac - Portfolio</title>
+	<script>
+		document.documentElement.className = "js";
+		var supportsCssVars = function() {
+			var e, t = document.createElement("style");
+			return t.innerHTML = "root: { --tmp-var: bold; }", document.head.appendChild(t), e = !!(window.CSS && window.CSS.supports && window.CSS.supports("font-weight", "var(--tmp-var)")), t.parentNode.removeChild(t), e
+		};
+		supportsCssVars() || alert("Please view this demo in a modern browser that supports CSS Variables.");
+	</script>
+</head>
